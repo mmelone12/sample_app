31ce64616eba9ec31cfd36b805dd573d0ac1a018
mmelone12 <matt.m.melone@gmail.com>
2011-09-03 19:50:54 +0200

    Done with static pages

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(:default, Rails.env) if defined? (Bundler)
 
 module SampleApp
   class Application < Rails::Application
@@ -37,6 +37,13 @@ module SampleApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password]     
+    ### Part of a Spork hack. See http://bit.ly/arY19Y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
